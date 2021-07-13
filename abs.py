77f06d74be49e1dc3a338ddfe50b0ddfe26b2561
v44k3 <vaakesan.sundrelingam@gmail.com>
2021-07-13 09:47:28 -0400

    fix final message bug

--- abs.py
+++ abs.py
@@ -140,7 +140,7 @@ class SymbolOfPeace(commands.Cog):
             await ctx.send(f'{i + 1} ...')
             await asyncio.sleep(1)
 
-        while time_remaining >= 0:
+        while time_remaining > 0:
             t = random.choices(self._times, self._weights)[0] * 60
             x = random.choice(self._exercises)
             self._exercises.remove(x)
